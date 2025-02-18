4727d5691d4ccab0f2844d7a9553b8c3f18efeba
Alix VEYRAT <alixveyrat@gmail.com>
2025-02-18 14:39:42 +0100

    1

--- game.js
+++ game.js
@@ -1,19 +1,20 @@
-import { quiz_numerique_responsable } from './questions.js'; // Import des questions
+import {quiz_numerique_responsable} from './questions.js'; // Import des questions
 
 // Récupérer les emplacements pour injecter la question et les options
-const __________ = document.getElementById('__________');
-const __________ = document.getElementById('__________');
+const questions = document.getElementById('question-text');
+const options = document.getElementById('options-container');
+console.log(quiz_numerique_responsable);
 
-// Récupérer la première question
-const firstQuestion = __________.questions[0];
+// // Récupérer la première question
+// const firstQuestion = __________.questions[0];
 
-// Injecter le texte de la question dans l'emplacement dédié
-__________.innerText = __________;
+// // Injecter le texte de la question dans l'emplacement dédié
+// __________.innerText = __________;
 
-// Pour chaque option, créer un bouton et l'ajouter au conteneur
-firstQuestion.__________.forEach(__________ => {
-  const __________ = document.createElement('button');
-  __________.innerText = __________;
-  __________.classList.add('__________');
-  __________.appendChild(__________);
-});
+// // Pour chaque option, créer un bouton et l'ajouter au conteneur
+// firstQuestion.__________.forEach(__________ => {
+//   const __________ = document.createElement('button');
+//   __________.innerText = __________;
+//   __________.classList.add('__________');
+//   __________.appendChild(__________);
+// });
